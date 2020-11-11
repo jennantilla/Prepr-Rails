d44a341de0a18c1cc3708e7b4ebcd463ba5ed4dd
jennantilla <jennantilla@gmail.com>
2020-11-10 23:57:46 -0800

    Titleize user entries and search

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,10 +1,14 @@
 class Inventory < ApplicationRecord
   belongs_to :user
   has_one_attached :img
+  before_save do |inventory|
+    inventory.item = inventory.item.titleize
+    inventory.category = inventory.category.titleize
+  end
 
   def self.search(search)
     if search
-      search_item = Inventory.find_by(item: search)
+      search_item = Inventory.find_by(item: search.titleize)
         if search_item
           self.where(id: search_item)
         else
